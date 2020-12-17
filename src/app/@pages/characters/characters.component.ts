7c0a9a4987a20515d59f89df0548124935f43705
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-17 11:39:51 +0100

    Añadir información de los personajes (nombre) en 3 columnas

--- src/app/@pages/characters/characters.component.ts
+++ src/app/@pages/characters/characters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICharacter } from 'src/app/@interfaces/character.interface';
 import { CharactersService } from './characters.service';
 
 @Component({
@@ -7,12 +8,13 @@ import { CharactersService } from './characters.service';
   styleUrls: ['./characters.component.css']
 })
 export class CharactersComponent implements OnInit {
-
+  charactersList: Array<ICharacter> = [];
   constructor(private characters: CharactersService) { }
 
   ngOnInit(): void {
     this.characters.list().subscribe((result) => {
       console.log(result);
+      this.charactersList = result;
     });
   }
 
