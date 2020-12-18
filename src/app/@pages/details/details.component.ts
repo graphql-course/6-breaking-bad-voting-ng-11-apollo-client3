66b0cd9d3f0df6c0c74b62f91ea9f1f173989fdb
Anartz Mugika Ledo <mugan86@gmail.com>
2020-12-18 12:05:21 +0100

    Add details in template to show correctly select character info

--- src/app/@pages/details/details.component.ts
+++ src/app/@pages/details/details.component.ts
@@ -1,6 +1,7 @@
 import { CharactersService } from './../characters/characters.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ICharacter } from 'src/app/@interfaces/character.interface';
 
 @Component({
   selector: 'app-details',
@@ -8,14 +9,18 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
-
+  character: ICharacter = {
+    id: '-1',
+    name: '',
+    votes: 0
+  };
   constructor(private router: ActivatedRoute, private charactersService: CharactersService) { }
 
   ngOnInit(): void {
     this.router.params.subscribe((params) => {
       const id = params.id;
       this.charactersService.get(id).subscribe((result) => {
-        console.log(result);
+        this.character = result;
       });
     });
   }
